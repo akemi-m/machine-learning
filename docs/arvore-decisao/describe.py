c4da156ab099247f75a08130578b1a5b8bdc39d7
akemi <akemi.mullis@gmail.com>
2025-08-18 09:27:56 -0300

    Atualização de requirements

--- docs/arvore-decisao/describe.py
+++ docs/arvore-decisao/describe.py
@@ -1,6 +1,6 @@
 import pandas as pd
+from tabulate import tabulate
 
 df = pd.read_csv('./docs/arvore-decisao/credit_score_classification.csv')
 
-from tabulate import tabulate
 print(tabulate(df.describe(), headers="keys", tablefmt="github"))
